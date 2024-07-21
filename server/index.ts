03efe632179d9de4facb1c4f506d26370fd33e83
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-07-21 12:08:23 +0530

    Post/put endpoints

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,30 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const types = require("/.types");
 app.use(express.json());
 
+app.post("/todo", function (req, res) {
+  const createPayload = req.body;
+  const parsedPayload = types.createTodo.safeParse(createPayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Please check the given input",
+    });
+    return;
+  }
+});
 
+app.get("/todos", function (req, res) {});
 
-app.post("/todo", function(req,res){
+app.put("/completed", function (req, res) {
+  const updatePayload = req.body;
+  const parsedPayload = types.updateTodo.safeParse(updatePayload);
+  if (!parsedPayload.success) {
+    res.status(411).json({
+      msg: "Please check the given input",
+    });
+    return;
+  }
+});
 
-})
-
-app.get("/todos", function(req, res){
-
-})
-
-app.put("/completed", function(req,res){
-
-})
-
-app.listen()
+app.listen();
